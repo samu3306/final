ba155276d2bad651551640ed98faaba6f4fcef46
samu3306 <coeall21809@gmail.com>
2025-06-03 20:02:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -395,5 +395,5 @@ def callback():
     return "OK"
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
